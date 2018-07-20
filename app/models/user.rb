05adb5c1503d9d8c27c8f956f1132d46dc3dd9da
AnabolicAction <itw9622@gmail.com>
2018-07-20 01:45:48 +0000

    model 관계설정

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+        #chatting 이랑 1:N
+         has_many :chat
+         #chat_rooms 이랑 N:M
+         has_many :admissions
+         has_many :chat_rooms, through: :admissions
 end
